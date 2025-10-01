b2f35346d8e2631df51072f6f5eb0480d2596a2f
john <elitejhn5@gmail.com>
2025-10-01 18:16:43 +0100

    added firebase and added game logic

--- lib/screens/questions.dart
+++ lib/screens/questions.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'package:firebase_database/firebase_database.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:quiz_app/screens/result.dart';
 
 class QuestionScreen extends StatefulWidget {
@@ -23,29 +24,33 @@ class _QuestionsScreenState extends State<QuestionScreen> {
   String optionB = '';
   String optionC = '';
   String optionD = '';
-  
 
   Future<void> getQuestions() async {
     DatabaseReference ref = FirebaseDatabase.instance.ref();
     final snapshot = await ref.child(questionNumber.toString()).get();
-    if (snapshot.exists){
+    if (snapshot.exists) {
       setState(() {
         question = snapshot.child('question').value.toString();
         answer = snapshot.child('answer').value.toString();
         optionA = snapshot.child('a').value.toString();
         optionB = snapshot.child('b').value.toString();
         optionC = snapshot.child('c').value.toString();
-        optionD = snapshot.child('d').value.toString();  
+        optionD = snapshot.child('d').value.toString();
       });
-    }else{
+    } else {
       print('-----Not found');
     }
-  } 
+  }
 
   Future<void> nextQuestion() async {
-    if (questionNumber > 6){
-      Navigator.push(context, MaterialPageRoute(builder: (context) => const ResultScreen(),));
-    }else{
+    if (questionNumber > 6) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const ResultScreen(),
+        ),
+      );
+    } else {
       await getQuestions();
     }
   }
@@ -57,23 +62,23 @@ class _QuestionsScreenState extends State<QuestionScreen> {
     loadQuestions = getQuestions();
   }
 
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.lightBlue, Colors.lightBlueAccent],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
+    Widget content = Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.lightBlue, Colors.lightBlueAccent],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
         ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Text(
                 question,
                 textAlign: TextAlign.center,
                 style: GoogleFonts.lato(
@@ -82,80 +87,105 @@ class _QuestionsScreenState extends State<QuestionScreen> {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              const SizedBox(
-                height: 50,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 5.0, left: 30, right: 30),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(double.infinity, 40)
-                  ),
-                  onPressed: () async {
-                    questionNumber++;
-                    await nextQuestion();
-                  },
-                  child: Text(
-                    textAlign: TextAlign.center,
-                    optionA,
-                    style: GoogleFonts.lato(color: Colors.blueAccent),
-                  ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 5.0, left: 30, right: 30),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(double.infinity, 40),
+                ),
+                onPressed: () async {
+                  questionNumber++;
+                  await nextQuestion();
+                },
+                child: Text(
+                  textAlign: TextAlign.center,
+                  optionA,
+                  style: GoogleFonts.lato(color: Colors.blueAccent),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 5.0, left: 30, right: 30),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(double.infinity, 40)
-                  ),
-                  onPressed: () async {
-                    questionNumber++;
-                    await nextQuestion();
-                  },
-                  child: Text(
-                    optionB,
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.lato(color: Colors.blueAccent),
-                  ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 5.0, left: 30, right: 30),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(double.infinity, 40),
+                ),
+                onPressed: () async {
+                  questionNumber++;
+                  await nextQuestion();
+                },
+                child: Text(
+                  optionB,
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.lato(color: Colors.blueAccent),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 5.0, left: 30, right: 30),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(double.infinity, 40)
-                  ),
-                  onPressed: () async {
-                    questionNumber++;
-                    await nextQuestion();
-                  },
-                  child: Text(
-                    textAlign: TextAlign.center,
-                    optionC,
-                    style: GoogleFonts.lato(color: Colors.blueAccent),
-                  ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 5.0, left: 30, right: 30),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(double.infinity, 40),
+                ),
+                onPressed: () async {
+                  questionNumber++;
+                  await nextQuestion();
+                },
+                child: Text(
+                  textAlign: TextAlign.center,
+                  optionC,
+                  style: GoogleFonts.lato(color: Colors.blueAccent),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 5.0, left: 30, right: 30),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(double.infinity, 40)
-                  ),
-                  onPressed: () async {
-                    questionNumber++;
-                    await nextQuestion();
-                  },
-                  child: Text(
-                    textAlign: TextAlign.center,
-                    optionD,
-                    style: GoogleFonts.lato(color: Colors.blueAccent),
-                  ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 5.0, left: 30, right: 30),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(double.infinity, 40),
+                ),
+                onPressed: () async {
+                  questionNumber++;
+                  await nextQuestion();
+                },
+                child: Text(
+                  textAlign: TextAlign.center,
+                  optionD,
+                  style: GoogleFonts.lato(color: Colors.blueAccent),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
+      ),
+    );
+    return Scaffold(
+      body: FutureBuilder(
+        future: loadQuestions,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.lightBlue, Colors.lightBlueAccent],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              ),
+              child: Center(
+                child: LoadingAnimationWidget.staggeredDotsWave(
+                  color: Colors.blueAccent,
+                  size: 150,
+                ),
+              ),
+            );
+          }
+          return content;
+        },
       ),
     );
   }
